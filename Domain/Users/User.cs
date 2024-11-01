7dd3dfcf429b5400e441f1eca6740faeeb80f82f
ruvim-ebs <ruvim.stratila@outlook.com>
2024-11-01 11:18:57 +0200

    Revert "ruvims coment"
    This reverts commit 6fa0a6ac0408884632f93a0fd6dac15562b01e92.

--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -23,7 +23,6 @@ namespace Domain.Users
 
         protected User(UserParams user)
         {
-            //coment
             FirstName = user.FirstName;
             LastName = user.LastName;
             Email = user.Email;
