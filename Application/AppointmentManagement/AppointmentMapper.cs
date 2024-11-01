66d219e67d6fc96921658ad60ed4e1e50134e3e8
nastiuskin <nasteagaydarly@gmail.com>
2024-11-01 14:33:46 +0200

    feat(Application): AddDurationToAppointmentMigration

---
+++ Application/AppointmentManagement/AppointmentMapper.cs
@@ -0,0 +1,16 @@
+﻿using Application.AppointmentManagement.DTO;
+using AutoMapper;
+using Domain.Appointments;
+
+namespace Application.AppointmentManagement
+{
+    public class AppointmentMapper : Profile
+    {
+        public AppointmentMapper() 
+        {
+            CreateMap<AppointmentCreateDto, Appointment>()
+                .ConstructUsing(src => Appointment.Create(src.DoctorId, src.PatientId, src.MedicalProcedureId,
+                src.Date, src.Duration).Value);
+        }
+    }
+}
